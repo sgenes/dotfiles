228d32d7fe0b62e6695e67b82709c84ecf958e53
ShinzJr <tamado.ipa1.29@gmail.com>
2016-09-12 09:42:33 +0700

    fix LS_COLORS

--- zsh/plugins/theme/theme.plugin.zsh
+++ zsh/plugins/theme/theme.plugin.zsh
@@ -1,7 +1,11 @@
 # ls colors
 autoload -U colors && colors
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
 # Enable ls colors
+if [ "$DISABLE_LS_COLORS" != "true" ]; then
+    ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+fi
 
 #setopt no_beep
 setopt auto_cd
