cb7dafda41cfcf2586f35b8b3e79ee8fa9f22a68
ShinzJr <tamado.ipa1.29@gmail.com>
2017-01-11 07:28:23 +0700

    tweak zsh config

--- zsh/plugins/history/history.plugin.zsh
+++ zsh/plugins/history/history.plugin.zsh
@@ -19,6 +19,6 @@ setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_all_dups # ignore duplication command history list
 # setopt hist_ignore_space
-setopt hist_verify
+# setopt hist_verify
 setopt inc_append_history
 setopt share_history # share command history data
